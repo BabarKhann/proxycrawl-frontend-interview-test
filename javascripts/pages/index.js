8a985a7d2b6a597b13b9eb4212b2ab06f99f0c74
babarkhan <babarkhan138@gmail.com>
2021-03-26 14:13:43 +0500

    add validation on form submit and render errors

--- javascripts/pages/index.js
+++ javascripts/pages/index.js
@@ -1,5 +1,61 @@
-// Example...
-// Avoid using jquery if possible.
-$(document).ready(() => {
-  console.log('Index page');
+const form = document.querySelector('#crawlForm');
+const errorElm = document.querySelector('.errors');
+const token = document.querySelector('.token');
+const url = document.querySelector('.url');
+const spinner = document.querySelector('.spinner');
+const product = document.querySelector('.product');
+
+const prod_img = document.querySelector('.prod-img img');
+const prod_title = document.querySelector('.title');
+const prod_subTitle = document.querySelector('.sub-title');
+const prod_price = document.querySelector('.price');
+const prod_desc = document.querySelector('.desc');
+let errors = [];
+
+function renderErrors() {
+
+  errorElm.textContent = '';
+
+  errors.map((error) => {
+    let p = document.createElement(`p`);
+    let newError = document.createTextNode(error)
+    p.appendChild(newError);
+
+    errorElm.appendChild(p);
+  });
+
+  errorElm.classList.replace('hidden', 'visible');
+}
+
+function checkFields() {
+  errors = [];
+
+  if (token.value !== '' && url.value !== '') {
+
+    errors = [];
+    errorElm.classList.replace('visible', 'hidden');
+
+    return true
+  };
+
+  if (token.value === '') {
+    errors.push("* Token is Required");
+  }
+
+  if (url.value === '') {
+    errors.push("* URL is Required");
+  }
+
+  renderErrors();
+
+  return false;
+}
+
+
+// form submit
+form.addEventListener('submit', async function (e) {
+  e.preventDefault();
+
+  if (!checkFields()) return;
+
 });
